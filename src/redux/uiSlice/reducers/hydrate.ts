374a39f4fe7affe969d37531bd187d73c7848e4d
Markox36 <devmarkox36@gmail.com>
2023-09-27 00:08:48 +0200

    a

--- src/redux/uiSlice/reducers/hydrate.ts
+++ src/redux/uiSlice/reducers/hydrate.ts
@@ -1,4 +1,4 @@
-import { UiState } from "@/redux/types/uiSlice";
+import { UiState } from "@zero/redux/types/uiSlice";
 import { Draft, PayloadAction } from "@reduxjs/toolkit";
 
 const hydrate = (state: Draft<UiState>, action: PayloadAction<{ ui: UiState }>): UiState => ({
