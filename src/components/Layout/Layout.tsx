69a0f0c01e59a7f869b6afb5016b6fadddc02850
Markox36 <devmarkox36@gmail.com>
2023-09-27 00:20:52 +0200

    a

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,25 @@
 import React, { PropsWithChildren, useEffect } from "react";
 import Header from "../Header/Header";
 import LayoutStyled from "./LayoutStyled";
+import Head from "next/head";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 	const a = "a";
 
 	return (
 		<LayoutStyled>
+			<Head>
+				<title>kluzk - Faq</title>
+				<meta name="description" content="Faq" />
+				<link rel="icon" href="/images/klukz.ico" />
+				{/* Open Graph metadata */}
+				<meta property="og:title" content={'Klukz - Faq'} />
+				<meta property="og:description" content={'Klukz - Faq'} />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content="Klukz" />
+				<meta property="og:url" content={'https://klukz.info/'} />
+				<meta property="og:image" content="/images/klukz.ico" />
+			</Head>
 			<Header />
 			{children}
 		</LayoutStyled>
